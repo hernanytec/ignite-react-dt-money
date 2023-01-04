e2194d834734af3d98f1d4c2d916d91867c16bfe
hernany <hernanytec777@gmail.com>
2023-01-04 02:38:42 -0300

    config: setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { ThemeProvider } from "styled-components";
-import { Transactions } from "./pages/Transactions";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle/>
-    
-    <Transactions />
+      <GlobalStyle />
+
+      <Transactions />
     </ThemeProvider>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
-import logoImg from '../../assets/logo.svg';
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from '../../assets/logo.svg'
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
-export function Header(){
-  return <HeaderContainer>
-    <HeaderContent>
-      <img src={logoImg} alt="" />
-      <NewTransactionButton>Nova transação</NewTransactionButton>
-    </HeaderContent>
-  </HeaderContainer>
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
 }
