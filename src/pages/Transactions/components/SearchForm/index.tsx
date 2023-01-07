c68aa96ab5ad17819c1636fccdd7354548d23de5
hernany <hernanytec777@gmail.com>
2023-01-06 23:25:37 -0300

    feat: transactions search

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,7 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
+import { useContext } from 'react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
+import { TransactionsContext } from '../../../../contexts/TransactionContext'
 import { SearchFormContainer } from './styles'
 
 const searchFormSchema = z.object({
@@ -11,6 +13,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
   const {
     register,
     handleSubmit,
@@ -19,7 +23,9 @@ export function SearchForm() {
     resolver: zodResolver(searchFormSchema),
   })
 
-  function handleSearchTransaction(data: SearchFormInputs) {}
+  async function handleSearchTransaction(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
 
   return (
     <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
